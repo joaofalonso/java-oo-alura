b2cf55333b457d0fbc76d920147df52828f71f1d
Julio Alonso <julioalonso89@gmail.com>
2023-06-16 14:47:41 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("I've finisehd the 01 class and now I'm diving deeper in Java");
+
+        int ano = 2023;
+        double rate = 6.0;
+        boolean flag = false;
+        String name = "Movie Name";
+        System.out.println("Data 1: Data 1 Value");
+
+        //Operators
+
+        //add value in the current variable in run time
+        int number = 5;
+        int result = ++number;
+        System.out.println(number);
+        System.out.println(result);
+
+
+        int number2 = 5;
+        int result2 = number2++; // increment 1 AFTER the line
+        System.out.println(number2);
+        System.out.println(result2);
+
+    }
+}
