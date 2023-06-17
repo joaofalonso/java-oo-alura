677fc8f605040f5d9288d44ae98b66dd0b83abfd
Julio Alonso <julioalonso89@gmail.com>
2023-06-17 13:31:43 -0300

    finished the challenge before watch the tip videos

--- src/Chalenge.java
+++ src/Chalenge.java
@@ -4,7 +4,6 @@ public class Chalenge {
 
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
         String menu = """
                 Select a option:
@@ -27,35 +26,55 @@ public class Chalenge {
                 ************************************************************
                 """.formatted(clientName, balance);
         System.out.println(accountData);
+        String accountStatemnts = "Bank Statement";
         while(run){
             System.out.println(menu);
             int option = scanner.nextInt();
+
             switch (option){
                 case 1:
                     System.out.println(accountData);
                     break;
                 case 2:
+                    System.out.println("Insert a description for the movement.");
+                    String description = scanner.next();
                     System.out.println("Insert the amount to deposit");
                     double amount = scanner.nextDouble();
+
+                    accountStatemnts += "\n" + description + ":" + amount;
+                    System.out.println("Validating the process...");
                     balance += amount;
                     accountData = """
-                    ************************************************************
-                    Account Owner : %S
-                    Balance : R$ %S
-                    ************************************************************
-                    """.formatted(clientName, balance);
+                                ************************************************************
+                                Account Owner : %S
+                                Balance : R$ %S
+                                ************************************************************
+                                %S
+                                """.formatted(clientName, balance, accountStatemnts);
                     break;
 
                 case 3:
-                    System.out.println("Insert the amount to withdraw");
-                    amount = scanner.nextDouble(); // it already os created in the same scope. line 52
-                    balance -= amount;
-                    accountData = """
-                    ************************************************************
-                    Account Owner : %S
-                    Balance : R$ %S
-                    ************************************************************
-                    """.formatted(clientName, balance);
+                    System.out.println("Insert a description for the movement.");
+                    description = scanner.next();
+                    System.out.println("Insert the amount to deposit");
+                    amount = scanner.nextDouble();
+                    double result = balance - amount;
+
+                    if(result >= 0)
+                    {
+                        balance -= amount;
+                        accountStatemnts += "\n"+ description + " : -" + amount;
+                        accountData = """
+                                    ************************************************************
+                                    Account Owner : %S
+                                    Balance : R$ %S
+                                    ************************************************************
+                                    %S
+                                    """.formatted(clientName, balance, accountStatemnts);
+
+                    }else{
+                        System.out.println("The current value in your account is lower the value to withdraw.");
+                    }
                     break;
 
                 case 4:
